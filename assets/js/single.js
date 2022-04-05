b5c5b8bc9ada64f55a335a090de298ca466042fc
CBurton <burton.chris13@gmail.com>
2022-04-05 11:29:10 -0500

    created list issues feature

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,9 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning")
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function(repo) {
-    console.log(repo);
+    // console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
     fetch(apiUrl).then(function(response) {
@@ -9,10 +11,15 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
-            alert("There was a problem with you request!");
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
         };
     });
 };
@@ -52,5 +59,31 @@ var displayIssues = function(issues) {
     };    
 
 };
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
 
- getRepoIssues("facebook/react");
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+var getRepoName = function() {
+    // grab repo name form Url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName) {
+        // diesplay repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        // it no repo was given, redirect to home page
+        document.location.replace("./index.html");
+    }
+};
+
+getRepoName();
